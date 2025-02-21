cb78bbbc1b0eda4da1fceca4f21ac1250490aff8
GUILHERME ROSA LOPES <guilherme.rlopes6@senacsp.edu.br>
2025-02-20 21:29:29 -0300

    versao final

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 /**
- * 
- * 
+ * Processo principal
+ * Estudo do CRUD com MongoDB
  */
 
-//importaçao do modulo de coneçao (database.js)
+// importação do módulo de conexão (database.js)
 const { conectar, desconectar } = require('./database.js')
 
-//execuçao de aplicativo
+// importação do modelo de dados de clientes
+const clienteModel = require('./src/models/Clientes.js')
+
+// CRUD Create (função para adicionar um novo cliente)
+const criarCliente = async (nomeCli, foneCli) => {
+    try {
+        const novoCliente = new clienteModel(
+            {
+                nomeCliente: nomeCli,
+                foneCliente: foneCli
+            }
+        )
+            // a linha abaixo salva os dados do cliente no banco
+            await novoCliente.save()
+            console.log("Cliente adicionado com sucesso.")
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+// execução da aplicação
 const app = async () => {
     await conectar()
+    await criarCliente("Leandro Ramos", "99999-0000")
+    await criarCliente("Sirlene Sanches", "99999-1111")
     await desconectar()
-
 }
 
 console.clear()
